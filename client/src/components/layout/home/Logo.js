ad2cbc524c839fcd1a7acafb678c629254c6247f
unknown <xypnotica@gmail.com>
2019-05-19 04:37:58 +0200

    Header is almost done

--- client/src/components/layout/home/Logo.js
+++ client/src/components/layout/home/Logo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Logo = () => {
-  return <div className='home__logo'>Devxad</div>;
+  return (
+    <div className='home__logo'>
+      <span className='home__logo--span'>D</span>evxad
+    </div>
+  );
 };
 
 export default Logo;
--- client/src/components/layout/home/Nav.js
+++ client/src/components/layout/home/Nav.js
@@ -6,22 +6,34 @@ const Nav = () => {
     <nav className='home__nav'>
       <ul className='home__nav--list'>
         <li className='home__nav--item'>
-          <Link to='/'>Home</Link>
+          <Link to='/' className='home__nav--link active'>
+            Home
+          </Link>
         </li>
         <li className='home__nav--item'>
-          <Link to='/'>Services</Link>
+          <Link to='/' className='home__nav--link'>
+            Services
+          </Link>
         </li>
         <li className='home__nav--item'>
-          <Link to='/'>Work</Link>
+          <Link to='/' className='home__nav--link'>
+            Work
+          </Link>
         </li>
         <li className='home__nav--item'>
-          <Link to='/'>Portfolio</Link>
+          <Link to='/' className='home__nav--link'>
+            Portfolio
+          </Link>
         </li>
         <li className='home__nav--item'>
-          <Link to='/'>About</Link>
+          <Link to='/' className='home__nav--link'>
+            About
+          </Link>
         </li>
         <li className='home__nav--item'>
-          <Link to='/'>Contact</Link>
+          <Link to='/' className='home__nav--link'>
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
