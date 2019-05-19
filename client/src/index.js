4ddebf0250aadee84900ae4eab8a0660c16cbb43
unknown <xypnotica@gmail.com>
2019-05-19 23:15:49 +0200

    header done

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './sass/main.scss';
+import './App.css';
 
 import * as serviceWorker from './serviceWorker';
 
