e83719bed14f83828cae1683eca60f59501e5cb7
unknown <xypnotica@gmail.com>
2019-05-19 02:24:20 +0200

    Basic page setup done part 2

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './sass/main.css';
+import './sass/main.scss';
 
 import * as serviceWorker from './serviceWorker';
 
